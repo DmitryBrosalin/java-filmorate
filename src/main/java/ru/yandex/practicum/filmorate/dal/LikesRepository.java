6f5f647e26ff61c1fceefcc53a66b3c17ee02deb
Pavel Appolonov <app.pavel1998@gmail.com>
2024-11-23 16:51:58 +0300

    Реализация функциональности "Рекомендации"

---
+++ src/main/java/ru/yandex/practicum/filmorate/dal/LikesRepository.java
@@ -0,0 +1,22 @@
+package ru.yandex.practicum.filmorate.dal;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+import ru.yandex.practicum.filmorate.dal.mapper.UserFilmMapper;
+import ru.yandex.practicum.filmorate.dto.UserFilmDto;
+
+import java.util.Collection;
+
+@Repository
+public class LikesRepository extends BaseRepository<UserFilmDto> {
+    private static final String FIND_ALL_QUERY = "SELECT user_id, film_id FROM likes";
+
+    public LikesRepository(JdbcTemplate jdbc, UserFilmMapper mapper) {
+        super(jdbc, mapper);
+    }
+
+    public Collection<UserFilmDto> getAllLikes() {
+        return findMany(FIND_ALL_QUERY);
+    }
+
+}
